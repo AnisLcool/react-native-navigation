7c2f73b01c9561ac4982c68186f35fcc1a8be375
anis rbh <mohamedrbh519@gmail.com>
2022-05-06 15:17:07 +0100

    1- creation des screens et de la configuration initiale de la navigation

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Welcome from './screens/Welcome';
+import Home from './screens/Home';
+import Login from './screens/Login';
 export default function App() {
+  // NavigationContainer => BrowserRouter dans react (Router)
+  const { Screen, Navigator } = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen name='Welcome' component={Welcome} />
+        <Screen name='Login' component={Login} />
+        <Screen name='Home' component={Home} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
