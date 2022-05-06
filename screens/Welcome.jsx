c18ff698c1a827888044e4b8264b5e92415b1774
anis rbh <mohamedrbh519@gmail.com>
2022-05-06 15:42:01 +0100

    3 - ajout navigation dans welcome(vers Login)

--- screens/Welcome.jsx
+++ screens/Welcome.jsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const Welcome = () => {
+  // useNavigation retourne un objet qui nous permet d'utiliser un ensemble de méthodes pour la navigation
+  const navigation = useNavigation();
+  const goToLogin = () => {
+    navigation.navigate('Login')
+  }
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Welcome</Text>
+      {/* <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate()}> */}
+      <TouchableOpacity style={styles.btn} onPress={goToLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default Welcome
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'coral',
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 30
+  },
+  btn: {
+    backgroundColor: 'lightblue',
+    marginVertical: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 30
+  }
+})
